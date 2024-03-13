a9d463c4bed6f9257c259d630468bcc4e1afed0a
genos <yunusemreozturk1@hotmail.com>
2024-03-14 01:03:44 +0300

    -

--- socket/test/test.js
+++ socket/test/test.js
@@ -2,17 +2,20 @@ const { createServer } = require("node:http");
 const { Server } = require("socket.io");
 const ioc = require("socket.io-client");
 const db = require("../../db/db_connection");
+const {getRooms, saveRoom, findRoomById} = require("../../api/src/controller");
 require('dotenv').config();
 
 const port = process.env.PORT_SOCKET;
 
+const accessToken2 = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NWYyMWM3OTZmYjk1ZjA0ZDFhOGFkMzgiLCJuYW1lIjoiZW1yZUBnbWFpbC5jb20iLCJpYXQiOjE3MTAzNjU4MTcsImV4cCI6MTcxMDk3MDYxN30.1tGDZpgE3izYtmLQKERfB5KJwaQb2Zf8f4G8g6Mh_KDS7FvojdqwCHyMaogT9vJJzF1VZAuBLiio5tnXM6ihVw";
+const accessToken1 = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NWYyMWNhZjZmYjk1ZjA0ZDFhOGFkM2IiLCJuYW1lIjoieXVzdWZAZ21haWwuY29tIiwiaWF0IjoxNzEwMzY1ODcxLCJleHAiOjE3MTA5NzA2NzF9.lEK8MbrtEPahtY7dfnBtCfuDHNQWpjRpeHSznNxdc9rO09_gJpkbwDknK4E-SWdDiwho-axSGBGiXqL4VAVeNA";
 function waitFor(socket, event) {
     return new Promise((resolve) => {
         socket.once(event, resolve);
     });
 }
 
-describe("my awesome project", () => {
+describe("Socket Test", () => {
     let io, serverSocket, clientSocket;
 
     beforeAll((done) => {
@@ -35,12 +38,13 @@ describe("my awesome project", () => {
         clientSocket.disconnect();
     });
 
-    test("should work", (done) => {
-        clientSocket.on("hello", (arg) => {
-            expect(arg).toBe("world");
+    it("should emit rooms", async (done) => {
+        await saveRoom({type: 0});
+        clientSocket.on("rooms", async (arg) => {
+            expect(arg[0]._id).toBeDefined()
             done();
         });
-        serverSocket.emit("hello", "world");
+        serverSocket.emit('rooms', await getRooms(accessToken1));
     });
 
     test("should work with an acknowledgement", (done) => {
